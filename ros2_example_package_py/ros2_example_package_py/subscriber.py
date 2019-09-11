29fd3c949e1c88275ce6d3b869472b7d3d95d521
George Stavrinos <stavrinosgeo@gmail.com>
2019-09-11 21:08:19 +0300

    Updated according to new ROS2 standards

--- ros2_example_package_py/ros2_example_package_py/subscriber.py
+++ ros2_example_package_py/ros2_example_package_py/subscriber.py
@@ -1,13 +1,13 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
+from rclpy.qos import qos_profile_sensor_data
 
 class PythonSubscriber(Node):
 
     def __init__(self):
         super().__init__("ros2_example_subscriber")
-        self.sub = self.create_subscription(String, "ros2_check_string", self.callback)
+        self.sub = self.create_subscription(String, "ros2_check_string", self.callback, qos_profile=qos_profile_sensor_data)
 
     def callback(self, msg):
         print(msg.data)
